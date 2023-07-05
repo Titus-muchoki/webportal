0c66cb183353c3517d0dbff69977cd50d347d465
kajela <titoyut56@gmail.com>
2023-07-05 17:22:52 +0300

    add the initial files

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Analy from '../asset/data-analytics1.jpg'
+import Analy from '../asset/austin-distel-EMPZ7yRZoGw-unsplash.jpg'
 const Analytics = () => {
     return(
         <div className='w-full bg-white py-16 px-4'>
             <div className='max-w-[1240px] mx-auto md:grid-cols-2'>
-            <img src={Analy} alt="/" />
-            <div>
-            <p>DATA ANALYTICS DASHBOARD</p>
+            <img className='w-[500px] max-auto my-4' src={Analy} alt="/" />
+            <div className='flex flex-col justify-center'>
+            <p className='text-[#71e03e] font-bold'>DATA ANALYTICS DASHBOARD</p>
             <h1>Manage Data Analytics Centrally</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. 
