a4081d356f5590ce8f216c0de8ee937692e32a54
kajela <titoyut56@gmail.com>
2023-07-07 15:10:49 +0300

    add the initial files

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Single from "../asset/single.png"
 const Cards = () => {
     return(
-        <div>
-            
+        <div className='w-full py-[10rem] px-4 bg-white'>
+            <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
+        <div className='w-full'>
+            <img src={Single} alt='/' />
+        </div>
+            </div>
         </div>
     )
 }
