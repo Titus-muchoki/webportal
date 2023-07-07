2a8a050318793e18a39059e30a830cd51eca3588
kajela <titoyut56@gmail.com>
2023-07-07 17:11:53 +0300

    add the initial files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {
+    FaDribbbleSquare,
+    FaFacebookSquare,
+    FaGithubSquare,
+    FaInstagram,
+    FaTwitterSquare,
+  } from 'react-icons/fa';
+  
+const Footer = () => {
+    return(
+        <div></div>
+    )
+}
+export default Footer;
